0a3ad1277c78cc892e3e229a8d021371929e9997
Gavin Dean <gavin@deanfam.net>
2022-10-28 01:02:14 -0400

    Initial commit

---
+++ test/al2-dev-env.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Al2DevEnv from '../lib/al2-dev-env-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/al2-dev-env-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Al2DevEnv.Al2DevEnvStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

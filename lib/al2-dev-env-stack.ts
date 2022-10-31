668a47bb2eb767606081d9b6b88240162d2db6e4
Gavin Dean <gavin@deanfam.net>
2022-10-31 10:20:17 -0700

    adds new connection

--- lib/al2-dev-env-stack.ts
+++ lib/al2-dev-env-stack.ts
@@ -4,12 +4,11 @@ import { Config } from '../lib/config';
 export class Al2DevEnvStack extends cdk.Stack {
   constructor(scope: Construct, id: string, config: Config, props?: cdk.StackProps) { super(scope, id, props);
     
-    const repo = new cdk.aws_codecommit.Repository(this, config.generalName.concat("Repository"),{
-      repositoryName: config.dashedName
-    });
     const pipeline = new cdk.pipelines.CodePipeline(this, config.generalName.concat("CodePipeline"), {
       synth: new cdk.pipelines.ShellStep("Synth", {
-        input: cdk.pipelines.CodePipelineSource.codeCommit(repo, "main"),
+        input: cdk.pipelines.CodePipelineSource.connection("Dean-Family/public-containers", "main", {
+          connectionArn: "arn:aws:codestar-connections:us-west-2:822585835475:connection/67f85605-5637-4317-b154-c3b3edcf900b"
+        }),
         commands: [
           "npm ci",
           "npm run build",
